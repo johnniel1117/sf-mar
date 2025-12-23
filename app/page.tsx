7f0d682b72754ca07ff2ab1d6ce1660b145ff5af
unknown <marjohnniel@gmail.com>
2025-12-23 14:43:03 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ export default function ExcelUploader() {
 
   const HaierLogo = () => (
     <img 
-      src="https://i.imgur.com/YXK7RZE.png" 
+      src="https://i0.wp.com/technode.com/wp-content/uploads/2024/11/%E6%88%AA%E5%B1%8F2024-11-20-17.05.54.png?fit=1696,1136&ssl=1" 
       alt="Haier Logo" 
       style={{ height: "40px", width: "auto" }}
     />
