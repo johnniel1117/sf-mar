57264e91bb5a59e0ea3c96626a693f7f83fbd618
Johnniel Mar <118031777+johnniel1117@users.noreply.github.com>
2026-02-06 09:50:16 -0800

    Update damage-report.tsx

--- components/damage-report.tsx
+++ components/damage-report.tsx
@@ -484,7 +484,6 @@ export default function DamageReportForm() {
           .title-section {
             text-align: right;
             align-items: center;
-            display: flex;
             flex-direction: column;
             gap: 10px;
           }
